d995aa0129966b7ef9b5eaf857aeac6e301fd625
Dominik242633 <242633@student.pwr.edu.pl>
2022-04-24 20:23:31 +0200

    change display message

--- lab2.py
+++ lab2.py
@@ -5,24 +5,26 @@ my_rank = comm.Get_rank()
 p = comm.Get_size()
 
 
-def oneToAllPersRing(sendMsg, sendSize, recvMsg):
-    """sendMsg - processor from we want to send message
-    sendSize - number of processors
-    recvMsg - to what processor we want to send message"""
+def oneToAllPersRing():
     message = ''
 
+    # If process 0, then send messages to 1 process - data from last to first
     if my_rank == 0:
         for i in range(p-1, 0, -1):
             comm.send(data[i], dest=1)
+            print(f"PROC 0 sent message {data[i]}")
         message = data[0]
 
+    # For process 1 to p-1 receive from previous process, then send to next process
     for i in range(1, p-1):
         if i == my_rank:
             for j in range(1, p-my_rank):
                 message = comm.recv(source=my_rank-1)
                 comm.send(message, dest=my_rank+1)
+                print(f"PROC {my_rank} received and sent message {message}")
             message = comm.recv(source=my_rank-1)
 
+    # If last process than only receive message
     if my_rank == p-1:
         message = comm.recv(source=my_rank-1)
 
@@ -30,10 +32,8 @@ def oneToAllPersRing(sendMsg, sendSize, recvMsg):
 
 
 if my_rank == 0:
-    data = [0, 1, 2, 3]
+    data = [0, 1, 2, 'aaa']
 
-oneToAllPersRing(sendMsg=0, sendSize=p, recvMsg=my_rank)
-# print(f"my_Rank: {my_rank}")
-# print(f"p: {p}")
+oneToAllPersRing()
 
 MPI.Finalize()
